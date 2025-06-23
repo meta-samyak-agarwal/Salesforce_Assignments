5a7552ae57d0e1336f740a20ecaa0ed0acbafdb8
meta-samyak-agarwal <samyak.agarwal@metacube.com>
2025-06-23 14:50:13 +0530

    Update javascriptAssignment.js

--- Javascript Assignment/javascriptAssignment.js
+++ Javascript Assignment/javascriptAssignment.js
@@ -9,6 +9,8 @@ function findMin(...args) {
   } 
   
 
+  console.log(findMin(5, 2, 9, -12, 8));
+
   // 2) Create a class Animal with a constructor setting the name property. Then, create 
   // a subclass dog that extendsAnimal and adds a bark method which prints dog name.(use prototype-based inheritance)
   // Define the Animal constructor (Animal class)
@@ -30,18 +32,23 @@ function findMin(...args) {
   Dog.prototype.bark = function() {
     console.log(this.name + " says Woof!");
   };
+
+  let myDogo = new Dog('Heaven');
+  myDogo.bark();
   
 
 
   // 3) Write a function multiplyByEight, Note: Use Closures, don't return 8*x directly.
   
-  function multiplyByEight(){
-      const multiplier = 8;
-      return function(num){
-          return num*multiplier;
+      function multiplyByEight(){
+          const multiplier = 8;
+          return function(num){
+              return num*multiplier;
+          }
       }
-  }
 
+      let result = multiplyByEight();
+      console.log(result(6));
 
   // 4) Create a function waitAndReturn that returns a Promise which resolves with the 
   // string "Resolved" after 5 seconds. Then, use async/await to call this function
@@ -49,33 +56,38 @@ function findMin(...args) {
   
 
   //--> a promise is a way to handle async calls
-  function waitAndReturn(){
-      return new Promise((resolve)=>{
-          setTimeout(() => { 
-                  resolve("Resolved");
-             }, 5000);
-      });
-  }
+    function waitAndReturn(){
+        return new Promise((resolve)=>{
+            setTimeout(() => { 
+                    resolve("Resolved");
+              }, 5000);
+        });
+    }
+
   
-  //-> the wait keyword is used to wait for a promise to resolve
-  async function print(){
-      str = await waitAndReturn();
-  
-      console.log(str);
-  }
+  // //-> the wait keyword is used to wait for a promise to resolve
+      async function print(){
+          str = await waitAndReturn();
+      
+          console.log(str);
+      }
+
+      print();
   
 
   // 5) Create a function to mutate an array with filter out values specified. (Hint : Use filter(), includes(), push())
   //    const filteredArray = (originalArray, removeValuesArray) => {//}
 
-  const filteredArray = (currentArray, removeElementArr)=>{
-    return currentArray.filter((element)=> !removeElementArr.includes(element));
-  }
+    const filteredArray = (currentArray, removeElementArr)=>{
+      return currentArray.filter((element)=> !removeElementArr.includes(element));
+    }
+
+ 
   
 
-  // test this above wala
+  // // test this above wala
 
-  const result = filteredArray([1,2,3,4,5,6,7,8], [4,5,6]);
+  const result2 = filteredArray([1,2,3,4,5,6,7,8], [4,5,6]);
   
-  console.log(result);
+  console.log(result2);
 
